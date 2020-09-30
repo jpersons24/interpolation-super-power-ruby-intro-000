909fb4dbe4c0bfd0a65c5b999ba29fdbd6a04d2d
carefree-window-8613 <jakobep24@gmail.com>
2020-09-30 20:52:26 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,9 @@
 # Write your #display_rainbow method here
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
 
+def display_rainbow(color)
+  puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
+end
 
 
 bob = ["Bob", 46, "Juggler", "555-555-5555"]
