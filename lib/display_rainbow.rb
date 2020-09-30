f4ea8f460ea147c1aae26a28e74eccefc4be9f83
carefree-window-8613 <jakobep24@gmail.com>
2020-09-30 20:30:47 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,9 @@
 # Write your #display_rainbow method here
+
+
+
+bob = ["Bob", 46, "Juggler"]
+stefani = ["Stefani", 49, "Firefighter"]
+
+puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}"
+puts "Name: #{stefani[0]}, Age: #{stefani[1]}, Occupation: #{stefani[2]}"
