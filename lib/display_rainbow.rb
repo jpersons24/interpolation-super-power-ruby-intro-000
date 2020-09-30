8ad20aa3073f7123bc52e9fe174e744b921559b5
carefree-window-8613 <jakobep24@gmail.com>
2020-09-30 20:33:06 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -7,3 +7,9 @@ stefani = ["Stefani", 49, "Firefighter"]
 
 puts "Name: #{bob[0]}, Age: #{bob[1]}, Occupation: #{bob[2]}"
 puts "Name: #{stefani[0]}, Age: #{stefani[1]}, Occupation: #{stefani[2]}"
+
+def print_business_card(person)
+  puts "Name: #{person[0]}, Age: #{person[2]}, Occupation: #{person{3}}"
+end
+
+print_business_card(bob)
