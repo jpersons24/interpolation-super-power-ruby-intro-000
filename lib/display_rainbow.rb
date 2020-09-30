5377028fb29614a44befa42a9a327612c7e29262
carefree-window-8613 <jakobep24@gmail.com>
2020-09-30 20:53:24 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -4,6 +4,8 @@ def display_rainbow(color)
   puts "R: #{color[0]}, O: #{color[1]}, Y: #{color[2]}, G: #{color[3]}, B: #{color[4]}, I: #{color[5]}, V: #{color[6]}"
 end
 
+display_rainbow(colors)
+
 
 bob = ["Bob", 46, "Juggler", "555-555-5555"]
 stefani = ["Stefani", 49, "Firefighter", "555-111-1111"]
